aabdd096dada7cd93042977679f02b4933b98278
Donghun Won <wonhun1225@gmail.com>
2024-07-22 13:28:31 +0900

    Fix: ci 부분 삭제 (#17)
    * Refactor : sonar qube ci 제거
    
    * Refactor : ci 부분 삭제
    
    * Style : 코드 포맷팅

--- src/main/java/com/quantweb/springserver/common/exception/CustomException.java
+++ src/main/java/com/quantweb/springserver/common/exception/CustomException.java
@@ -2,7 +2,6 @@ package com.quantweb.springserver.common.exception;
 
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatusCode;
 
 @Getter
 @RequiredArgsConstructor
--- src/main/java/com/quantweb/springserver/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/quantweb/springserver/common/exception/CustomExceptionHandler.java
@@ -1,13 +1,11 @@
 package com.quantweb.springserver.common.exception;
 
+import com.quantweb.springserver.common.entity.CustomResponseEntity;
 import org.json.JSONObject;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-
-import com.quantweb.springserver.common.entity.CustomResponseEntity;
 
 @RestControllerAdvice
 public class CustomExceptionHandler {
