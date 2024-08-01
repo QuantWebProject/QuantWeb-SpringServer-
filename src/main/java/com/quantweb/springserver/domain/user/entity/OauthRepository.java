f0bcfb5cc472e76eaad40ad39c1b6ea729482208
KimChungEon <109846781+KimChungEon@users.noreply.github.com>
2024-08-01 14:59:23 +0900

    Revert "Develop -> main 병합"

--- src/main/java/com/quantweb/springserver/domain/user/entity/OauthRepository.java
+++
@@ -1,7 +0,0 @@
-package com.quantweb.springserver.domain.user.entity;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OauthRepository extends JpaRepository<Oauth, Long> {
-
-}
